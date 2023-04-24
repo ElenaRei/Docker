31d3e8ac125b561dc2ef24fa5a4b3c1002715a83
Elena Reibling <e.rebling@googlemail.com>
2023-04-24 10:57:19 +0200

    HW1 done

---
+++ HW1.js
@@ -0,0 +1,20 @@
+// Описание задания.
+
+//  1 уровень сложности: 1. Установить docker на ваш ПК
+// https://docs.docker.com/desktop/
+
+// Установка и обновление WSL2:
+// https://learn.microsoft.com/en-us/windows/wsl/install-manual
+// Включение функций виртуализации:
+// https://docs.docker.com/desktop/troubleshoot/topics/#virtualization
+// Включение виртуализации в BIOS:
+// https://bce.berkeley.edu/enabling-virtualization-in-your-pc-bios.html
+
+
+
+// Запустить в консоли команду:
+// docker -v 
+// - shows version
+// docker run hello-world
+
+// HW 1 DONE!
